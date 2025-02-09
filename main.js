3be8d8a0e65a52a5e2998445e3da97b2de3d7a87
Eugine Ebenezer <eugineebenezer@gmai.com>
2025-02-09 21:50:52 +0530

    Route structure

--- main.js
+++ main.js
@@ -8,6 +8,24 @@ app.get('/', (req, res) =>{
     res.json({msg: "Hello All"})
 }) 
 
+
+app.get('/movies', ()=>{
+
+})
+
+app.post('/movies', ()=>{
+
+})
+
+app.put('/movies/:id', ()=>{
+
+})
+
+app.delete('/movies/:id', ()=>{
+
+})
+
+
 app.listen(Port, () =>{
     console.log(`The server is running at http://localhost:${Port} `);
 })
